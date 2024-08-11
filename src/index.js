712b6467e09b920438c1c955a221d1e06d90c5ea
sofiiahr01 <sgradinarova2001@gmail.com>
2024-08-12 00:49:34 +0300

    hw_11 completed

--- src/index.js
+++ src/index.js
@@ -7,43 +7,52 @@ const sport = prompt('Який Ваш улюблений вид спорту?');
 const currentYear = new Date().getFullYear();
 const age = currentYear - parseInt(yearBirth);
 
+let commonAlert = '';
+
 if (yearBirth !== null && !isNaN(yearBirth) && yearBirth.trim() !== "") {
-    alert('Вам приблизно ' + age + ' років.' );
+    commonAlert += 'Вам ' + age + ' років.\n';
 } else {
-    alert("Шкода, що Ви не захотіли ввести свій рік народження.");
+    commonAlert += "Шкода, що Ви не захотіли ввести свій рік народження.\n";
 }
 
-if (city ===null || city.trim() === ''){
-    alert(`Шкода, що Ви не захотіли ввести своє місто.`)
+if (city === null || city.trim() === '') {
+    commonAlert += 'Шкода, що Ви не захотіли ввести своє місто.\n';
 } else {
-    switch (city){
+    switch (city) {
         case 'Київ':
-            alert('Ви проживаєте у столиці України.');
+            commonAlert += 'Ви проживаєте у столиці України.\n';
             break;
         case 'Лондон':
-            alert('Ви проживаєте у столиці Англії.');
+            commonAlert += 'Ви проживаєте у столиці Англії.\n';
             break;
         case 'Вашингтон':
-            alert('Ви проживаєте у столиці США.');
+            commonAlert += 'Ви проживаєте у столиці США.\n';
             break;
         default:
-            alert(`Ви проживаєте у місті ${city}.`);
-    }}
-
-if (sport ===null || sport.trim() === ''){
-    alert('Шкода, що Ви не захотіли ввести свій улюблений вид спорту.')
-} else {
-    switch (sport){
-        case 'бокс':
-            alert('Круто! Хочете стати Олександром Усиком?');
-            break;
-        case 'фехтування':
-            alert('Круто! Хочете стати Ольгою Харлан?');
-            break;
-        case 'шахмати':
-            alert('Круто! Хочете стати Доммараджу Гукешем?');
-            break;
+            commonAlert += `Ви проживаєте у місті ${city}.\n`;
     }
 }
+
+if (sport === null || sport.trim() === '') {
+    commonAlert += 'Шкода, що Ви не захотіли ввести свій улюблений вид спорту.\n';
+} else {
+    switch (sport) {
+        case 'бокс':
+            commonAlert += 'Круто! Хочете стати Олександром Усиком?\n';
+            break;
+        case 'фехтування':
+            commonAlert += 'Круто! Хочете стати Ольгою Харлан?\n';
+            break;
+        case 'шахмати':
+            commonAlert += 'Круто! Хочете стати Доммараджу Гукешем?\n';
+            break;
+        default:
+            commonAlert += `Ваш улюблений вид спорту ${sport}.\n`;
+    }
+}
+
+alert(commonAlert)
+
+
 
 
