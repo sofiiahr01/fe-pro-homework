f091440ab5fbcfa769c708da4188f24d8cd5c898
sofiiahr01 <sgradinarova2001@gmail.com>
2024-08-11 18:10:31 +0300

    hw_11 completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 'use strict';
 
-const userName = prompt('Enter user name')
+let numOrStr = prompt('input number or string');
+console.log(numOrStr)
 
-alert(`Hello ${userName}! How are you?`);
+switch (true){
+    case numOrStr === null:
+        console.log('ви скасували');
+        break;
+    case numOrStr.trim() === '':
+        console.log('Empty String');
+        break;
+    case isNaN( +numOrStr ):
+        console.log(' number is Ba_NaN');
+        break;
+    default:
+        console.log('OK!')
+}
