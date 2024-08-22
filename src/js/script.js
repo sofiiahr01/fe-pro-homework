c6756c6ca0e7145c99f4d5e6a6e74b01df395451
sofiiahr01 <sgradinarova2001@gmail.com>
2024-08-22 21:59:20 +0300

    hw_16 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,33 @@
 'use strict';
 
-const userName = prompt('Enter user name')
+function padString(str, stringLenght, symbol, addLeft = false){
+    if (typeof str !== 'string' ){
+        return 'string error'
+    }
+    if (typeof stringLenght !=='number'){
+        return 'stringLenght error'
+    }
+    if (typeof symbol !== 'string' || symbol.length !== 1){
+        return 'symbol error'
+    }
+    if (typeof addLeft !== 'boolean'){
+        return 'boolean error'
+    }
+    if (str.length >= length) {
+        return str.substr(0, length);
+    }
+    const addSymbol = length - str.length;
+    const add = symbol.repeat(addSymbol);
+    if (addSymbol) {
+        return add + str;
+    } else {
+        return str + add;
+    }
 
-alert(`Hello ${userName}! How are you?`);
+}
+
+
+
+
+
+console.log(padString())
