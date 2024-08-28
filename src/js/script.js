5831a0aab3f74f4a29cd15476fbd0b88e1e66e4d
sofiiahr01 <sgradinarova2001@gmail.com>
2024-08-28 15:37:19 +0300

    hw_17 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,11 @@
 'use strict';
 
-const userName = prompt('Enter user name')
+function removeElement(array, item){
+    const index = array.indexOf(item)
+    if (index !== -1) {
+    array.splice(index, 1)}
+}
 
-alert(`Hello ${userName}! How are you?`);
+const array = [1, 2, 3, 4, 5, 6, 7];
+removeElement(array, 5 );
+console.log(array);
