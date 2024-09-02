b5e460fc19771d52a04f138d4dcdf774b9265b9c
sofiiahr01 <sgradinarova2001@gmail.com>
2024-09-03 01:41:51 +0300

    hw_19 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,32 @@
 'use strict';
 
-function exponentiation(num, power){
-    const result = Math.pow(num, power)
-    alert(result)
+function exponentiation(num1, num2){
+    const result = Math.pow(num1, num2)
+    alert(`${num1} в ступені ${num2} дорівнює ${result}`)
 }
 
-function mainFunction(callback){
-    let num = +prompt('Введіть число:')
-    let power = +prompt('Введіть ступінь:')
-    callback(num, power)
+function multiplay(num1, num2){
+    const multiplication = num1 * num2
+    alert(`${num1} помножене на ${num2} дорівнює ${multiplication}`)
 }
-mainFunction(exponentiation)
+
+function division(num1, num2){
+    const divide = num1 / num2
+    alert(`${num1} поділене на ${num2} дорівнює ${divide}`)
+}
+
+function modulo(num1, num2){
+    const mod = num1 % num2
+    alert(`Залишок від поділу ${num1} на ${num2} дорівнює ${mod}`)
+}
+
+function mainFunction(callback1, callback2, callback3,callback4){
+    const num1 = +prompt('Введіть перше число:')
+    const num2 = +prompt('Введіть друге число:')
+    callback1(num1, num2)
+    callback2(num1, num2)
+    callback3(num1, num2)
+    callback4(num1, num2)
+}
+mainFunction(exponentiation, multiplay, division, modulo)
+
