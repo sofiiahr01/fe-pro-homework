b4b02a8a4788440f79ec946df39bcd22df908100
sofiiahr01 <sgradinarova2001@gmail.com>
2024-09-04 19:22:59 +0300

    hw_21 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,19 @@
 'use strict';
 
-const userName = prompt('Enter user name')
+const arrLength = +prompt("Введіть довжину масиву:");
 
-alert(`Hello ${userName}! How are you?`);
+let arr = [];
+
+for (let i = 0; i < arrLength; i++) {
+    let element = prompt(`Введіть елемент ${i + 1}:`);
+    arr.push(element);
+}
+
+alert(arr);
+
+alert(arr.sort(function (a, b) {
+    return a - b;
+}));
+
+arr.splice(1, 3)
+alert(arr);
