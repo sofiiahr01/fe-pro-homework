80be9870b2bb1b01042d0d9778a2a4d64ff16fd4
sofiiahr01 <sgradinarova2001@gmail.com>
2024-09-04 20:44:40 +0300

    hw_22 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,24 @@
 'use strict';
 
-const userName = prompt('Enter user name')
+const arr = [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
 
-alert(`Hello ${userName}! How are you?`);
+const positive = arr.filter(number => number > 0)
+console.log(`Кількість позитивних елементів в масиві - ${positive.length}.`);
+const positiveSum = positive.reduce((accumulator, currentValue) => accumulator + currentValue);
+console.log(`Сума позитивних елементів дорівнює ${positiveSum}.`);
+
+/*const minNumber = arr.reduce((sum, current) => sum > current ? current : sum);
+console.log(minNumber);
+const indexMinNum = minNumber.indexOf;
+console.log(indexMinNum)
+
+const maxNumber = arr.reduce((sum, current) => sum < current ? current : sum);
+console.log(maxNumber);
+const indexMaxNum = maxNumber.indexOf;
+console.log(indexMaxNum)*/
+
+const negative = arr.filter(number => number < 0)
+console.log(`Кількість негативних елементів в масиві - ${negative.length}.`);
+
+const uneven = arr.filter(number => number % 2 === 1 )
+console.log(`Кількість позитивних непарних елементів в масиві - ${uneven.length}.`);
