c7320a0ef6854ed947419ff2072920c084fddbd0
sofiiahr01 <sgradinarova2001@gmail.com>
2024-09-06 20:52:40 +0300

    hw_22 completed

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,7 @@
 
 const arr = [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
 
-const positive = arr.filter(number => number > 0)
+const positive = arr.filter(number => number >= 0)
 console.log(`Кількість позитивних елементів в масиві - ${positive.length}.`);
 const positiveSum = positive.reduce((accumulator, currentValue) => accumulator + currentValue);
 console.log(`Сума позитивних елементів дорівнює ${positiveSum}.`);
