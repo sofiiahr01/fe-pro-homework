617b39ff32551fdf4542029853851fba2d4794d4
sofiiahr01 <sgradinarova2001@gmail.com>
2024-09-15 19:40:24 +0300

    hw_24 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,30 @@
 'use strict';
 
-const userName = prompt('Enter user name')
+const variablesExample = () => {
+    console.log(firstVariable)
+    console.log(secondVariable)
+    console.log(thirdVariable)
+    for (let i = 0; i < 3; i++){
+        var firstVariable = i;
+        let secondVariable = i;
+        const thirdVariable = i;
+        console.log(firstVariable)
+        console.log(secondVariable)
+        console.log(thirdVariable)
+    }
+    console.log(firstVariable)
+    console.log(secondVariable)
+    console.log(thirdVariable)
+}
+variablesExample()
 
-alert(`Hello ${userName}! How are you?`);
+/* Після хостингу var підіймається на початок. При виведенні змінної var в консоль до
+її ініціалізації отримаємо undefined. Змінні let та const також підіймаються вгору, але
+потрапляють в Temporal Dead Zone, тобто поки код не дійде до оголошення цих змінніх, вони
+будуть недоступні.
+В консолі побачимо виконання кожної ітерації, а також значення змінної var, яку ми виводили
+після завершення циклу. Вивелось значення тільки цієї змінної, бо вона має глобальну область
+видимості та може існувати як в циклі, так і поза нього. А змінні let і const доступні
+тільки всередині циклу, тому що мають блочну область видимості.
+*/
+
