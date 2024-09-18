824f3e53d7b2fa3643924e8085974fae4b4f10a5
sofiiahr01 <sgradinarova2001@gmail.com>
2024-09-18 22:42:28 +0300

    hw_28 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,36 @@
 'use strict';
 
-const userName = prompt('Enter user name')
+//Task 1
+const factorial = function(n) {
+    if (n === 1) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
 
-alert(`Hello ${userName}! How are you?`);
+console.log(factorial(3))
+
+//Task 2
+const pow = function(num, degree) {
+    if (degree === 1) {
+        return num;
+    } else {
+        return num * pow(num, degree - 1);
+    }
+}
+
+console.log(pow(4, 2))
+
+//Task 3
+const sum = function(a, b) {
+    if (b === 0) {
+        return a;
+    } else {
+        return sum(a + 1, b - 1);
+    }
+}
+
+console.log(sum(9, 4));
+
+
+
