8d649a65b142970cbc2ac4b37c802dac69da26a3
sofiiahr01 <sgradinarova2001@gmail.com>
2024-09-20 17:07:41 +0300

    hw_27 update

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,23 @@
 'use strict';
 
+const bindFunc = function(func, context, ...linkedArgs) {
+    return function(...args) {
+        return func.bind(context)(...linkedArgs, ...args);
+    };
+}
+
 const userName = {
     name: 'Yana'
 }
 
-const greeting = function () {
-    console.log('Welcome, ' + this.name)
+const greet = function(greeting) {
+    return `${greeting}, ${this.name}`;
 }
 
-let userGreeting = greeting.bind(userName)
-userGreeting()
+const boundGreet = bindFunc(greet, userName, 'Welcome');
+console.log(boundGreet())
+
+
 
 
 
