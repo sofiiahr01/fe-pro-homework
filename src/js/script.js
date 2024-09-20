45d7849c8a0e2bdd4f3810dcad8b6e4e543836d4
sofiiahr01 <sgradinarova2001@gmail.com>
2024-09-20 16:50:39 +0300

    hw_26 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,33 @@
 'use strict';
 
-const userName = prompt('Enter user name')
+function insertWordInto(str) {
+    const words = str.split(' ')
+    let insertIndex = 0
 
-alert(`Hello ${userName}! How are you?`);
+    return function(word) {
+        const gap = ` ${word} `
+        if (insertIndex === 0) {
+            words.unshift(gap.trim())
+        } else if (insertIndex === words.length) {
+            words.push(gap.trim())
+        } else {
+            words[insertIndex - 1] += gap
+        }
+
+        insertIndex++;
+
+        if (insertIndex > words.length) {
+            insertIndex = 0;
+        }
+
+        return words.join(' ').trim();
+    };
+}
+
+const paste = insertWordInto('hello world');
+console.log(paste('Odesa'));
+console.log(paste('Odesa'));
+console.log(paste('Odesa'));
+console.log(paste('Odesa'))
+
+
