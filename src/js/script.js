afce8eff71a6b045d75e398b4a1c52d12c352d41
sofiiahr01 <sgradinarova2001@gmail.com>
2024-09-21 20:21:09 +0300

    hw_29 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,27 @@
 'use strict';
 
-const userName = prompt('Enter user name')
+const complexArray = [
+    [1, 2, [3.1, 3.2, [3.21, 3.22]], 4],
+    [5, [6, [7, [8, 9]]]],
+    [[10, 11], 12, 13]
+];
 
-alert(`Hello ${userName}! How are you?`);
+console.log(complexArray)
+
+const flat = function(arr, result = []) {
+    if (arguments.length > 1) {
+        throw new Error('Function accepts only 1 argument, too much arguments provided');
+    }
+    for(const item of arr) {
+        if(Array.isArray(item)) flat(item, result);
+        else result.push(item);
+    }
+
+    return result;
+}
+
+try {
+    console.log(
+        flat(complexArray)
+    );
+} catch (error){console.error(error.message);}
