1df082c3d3abd8748533b09468ff788401bf2927
sofiiahr01 <sgradinarova2001@gmail.com>
2024-09-26 21:33:50 +0300

    hw_33 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,13 @@
 'use strict';
 
-const userName = prompt('Enter user name')
+const images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg'];
+const randomImgIndex = Math.floor(Math.random() * images.length);
+const randomImg = images[randomImgIndex];
+const imgPath = `./images/${randomImg}`;
+const resultImg = document.createElement('img');
+resultImg.src = imgPath;
+document.body.appendChild(resultImg);
 
-alert(`Hello ${userName}! How are you?`);
+
+
+
