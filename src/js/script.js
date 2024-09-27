255d250bb10d8dd7bc9250ceb3dc690ff2342d04
sofiiahr01 <sgradinarova2001@gmail.com>
2024-09-27 17:50:59 +0300

    hw_32 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,22 @@
 'use strict';
 
-const userName = prompt('Enter user name')
 
-alert(`Hello ${userName}! How are you?`);
+const tableFunc = (rows, columns) => {
+    const table = document.getElementById('table');
+    let count = 1;
+
+    for (let i = 0; i < rows; i++) {
+        const row = document.createElement('tr');
+
+        for (let j = 0; j < columns; j++) {
+            const cell = document.createElement('td');
+            cell.textContent = count;
+            row.appendChild(cell);
+            count++;
+        }
+
+        table.appendChild(row);
+    }
+}
+
+tableFunc(10, 10);
