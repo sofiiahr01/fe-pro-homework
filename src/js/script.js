3d7aedd31ee1006105087602a52a16c50903ff8b
sofiiahr01 <sgradinarova2001@gmail.com>
2024-09-28 16:05:35 +0300

    initial commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,26 @@
 'use strict';
 
-
-const tableFunc = (rows, columns) => {
-    const table = document.getElementById('table');
-    let count = 1;
-
-    for (let i = 0; i < rows; i++) {
-        const row = document.createElement('tr');
-
-        for (let j = 0; j < columns; j++) {
-            const cell = document.createElement('td');
-            cell.textContent = count;
-            row.appendChild(cell);
-            count++;
-        }
-
-        table.appendChild(row);
-    }
+const appendHTMLElement = (parentEl = null, elementToAppend = null) => {
+if (!parentEl || !elementToAppend) return
+    parentEl.append(elementToAppend)
 }
 
-tableFunc(10, 10);
+const generateTable = () =>{
+    const tableSize = 10;
+    const table = document.createElement('table');
+    const tbody = document.createElement('tbody');
+    table.append(tbody);
+    for (let i = 0; i <= tableSize; i++){
+        const tr = document.createElement('tr');
+        for (let c = 0; i <= tableSize; c++){
+        const td = document.createElement('td');
+         td.innerHTML = c +'' ;
+         tr.append(td);
+        }
+         tbody.append(tr);
+    }
+    return table;
+}
+
+console.log(generateTable())
+appendHTMLElement(document.body, generateTable())
