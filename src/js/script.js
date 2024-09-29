76a9de972c6f585bcf2aca569734c423aed9647f
sofiiahr01 <sgradinarova2001@gmail.com>
2024-09-30 01:11:24 +0300

    initial commit

--- src/js/script.js
+++ src/js/script.js
@@ -5,8 +5,28 @@ const appendHTMLElement = (parentEl = null, elementToAppend = null) => {
     parentEl.append(elementToAppend)
 
 }
+    const textFeild = document.createElement('input')
 
-const textFeild = document.createElement('input')
-console.log(textFeild)
+    textFeild.type = 'text';
+    textFeild.placeholder = 'Text';
 
+    document.body.append(textFeild)
+    const div = document.createElement('div')
+div.style.display = 'none';
+div.style.border = '1px solid #000';
+div.style.padding = '10px';
+div.style.marginTop = '10px';
+document.body.append(div);
+
+function showDiv() {
+    div.style.display = 'block';
+}
+
+function hideDiv() {
+    div.style.display = 'none';
+}
+
+textFeild.addEventListener('mouseover', showDiv);
+textFeild.addEventListener('mouseout', hideDiv);
 appendHTMLElement()
+
