f0fc4adf1cb9d87947c18e799aaafbaa876c1364
sofiiahr01 <sgradinarova2001@gmail.com>
2024-10-03 23:34:08 +0300

    hw_34 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,46 @@
 'use strict';
 
-    const showHideDiv = () => {
-        const textFeild = document.createElement('input')
-        textFeild.type = 'text';
-        textFeild.placeholder = 'Text';
-        document.body.append(textFeild)
+const appendHTMLElement = (textField, div) => {
+    document.body.append(textField, div);
+};
 
-        const div = document.createElement('div')
-        div.innerHTML = 'Hello :)'
-        document.body.append(div);
+const createInput = () => {
+    const textField = document.createElement('input');
+    textField.type = 'text';
+    textField.placeholder = 'Text';
+    return textField;
+};
 
-        textFeild.addEventListener('mouseover', () => {
-            div.style.display = 'block';
-        });
-        textFeild.addEventListener('mouseout', () => {
-            div.style.display = 'none';
-        });
+const createDiv = () => {
+    const div = document.createElement('div');
+    div.innerHTML = 'Hello :)';
+    div.classList.add('hidden');
+    return div;
+};
 
-    }
+const addEventListeners = (textField, div) => {
+    textField.addEventListener('mouseover', () => {
+        div.classList.remove('hidden');
+        div.classList.add('show');
+    });
+    textField.addEventListener('mouseout', () => {
+        div.classList.add('hidden');
+        div.classList.remove('show');
+    });
+};
 
-showHideDiv()
+const showHideDiv = () => {
+    const textField = createInput();
+    const div = createDiv();
+    appendHTMLElement(textField, div);
+    addEventListeners(textField, div);
+};
+
+showHideDiv();
+
+
+
+
+
+
 
