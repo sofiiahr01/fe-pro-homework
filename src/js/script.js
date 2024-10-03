32632f5b29a351323dd97645e7a1ec9cf5fe8a20
sofiiahr01 <sgradinarova2001@gmail.com>
2024-10-03 23:51:32 +0300

    hw_33 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,22 @@
 'use strict';
 
 const images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg'];
-const randomImgIndex = Math.floor(Math.random() * images.length);
-const randomImg = images[randomImgIndex];
-const imgPath = `./images/${randomImg}`;
-const resultImg = document.createElement('img');
-resultImg.src = imgPath;
-document.body.append(resultImg);
+
+const getRandomImage = (imagesArray) => {
+    const randomImgIndex = Math.floor(Math.random() * imagesArray.length);
+    return imagesArray[randomImgIndex];
+}
+
+const randomImage = () => {
+    const randomImg = getRandomImage(images);
+    const imgPath = `./images/${randomImg}`;
+    const resultImg = document.createElement('img');
+    resultImg.src = imgPath;
+    document.body.append(resultImg);
+}
+
+randomImage();
+
 
 
 
