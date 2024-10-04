397961c631bc2a4c770a2495738f4de2a060feba
sofiiahr01 <sgradinarova2001@gmail.com>
2024-10-04 18:58:05 +0300

    hw_32 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,32 @@
 'use strict';
 
-const userName = prompt('Enter user name')
+const appendHTMLElement = (parentEl = null, elementToAppend = null) => {
+    if (!parentEl || !elementToAppend) return;
+    parentEl.append(elementToAppend);
+};
 
-alert(`Hello ${userName}! How are you?`);
+const createCounter = () => {
+    let count = 1;
+    return () => count++;
+};
+
+const generateTable = (rows, cols) => {
+    const table = document.createElement('table');
+    const tbody = document.createElement('tbody');
+    table.append(tbody);
+
+    const cellCounter = createCounter()
+
+    for (let i = 0; i < rows; i++) {
+        const tr = document.createElement('tr');
+        for (let c = 0; c < cols; c++) {
+            const td = document.createElement('td');
+            td.textContent = cellCounter();
+            tr.append(td);
+        }
+        tbody.append(tr);
+    }
+    return table
+}
+
+appendHTMLElement(document.body, generateTable(10, 10))
