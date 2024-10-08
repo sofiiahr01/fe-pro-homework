649248a4fd5efdbbf2951f27ae914abd3fdb8c0b
sofiiahr01 <sgradinarova2001@gmail.com>
2024-10-08 20:30:05 +0300

    hw_35 completed

--- src/js/solution.js
+++ src/js/solution.js
@@ -1,52 +1,54 @@
 'use strict';
 
-const ulElement = document.querySelector('ul');
-const liElements = document.querySelectorAll('ul li');
-const ulAttr = ulElement.attributes;
+(function() {
+    const ulElement = document.querySelector('ul');
+    const liElements = document.querySelectorAll('ul li');
+    const ulAttr = ulElement.attributes;
 
-const liText = () => {
-    liElements.forEach((li) => {
-        console.log(li.textContent);
-    });
-}
-
-const getUlVal = () => {
-    const attrValues = [];
-
-    for (let attr of ulAttr) {
-        attrValues.push(attr.value);
+    const liText = () => {
+        liElements.forEach((li) => {
+            console.log(li.textContent);
+        });
     }
-    console.log(attrValues);
-}
 
-const getUlNames = () => {
-    const attrNames = [];
+    const getUlVal = () => {
+        const attrValues = [];
 
-    for (let attr of ulAttr) {
-        attrNames.push(attr.name);
+        for (let attr of ulAttr) {
+            attrValues.push(attr.value);
+        }
+        console.log(attrValues);
     }
-    console.log(attrNames);
-}
 
-const changeLastLiText = (newText) => {
-    liElements[liElements.length - 1].textContent = newText;
-}
+    const getUlNames = () => {
+        const attrNames = [];
 
-const addAttrToFirstLi = (name) => {
-    liElements[0].setAttribute('data-my-name', name);
-}
+        for (let attr of ulAttr) {
+            attrNames.push(attr.name);
+        }
+        console.log(attrNames);
+    }
 
-const removeAttrFunc = () => {
-    ulElement.removeAttribute('data-dog-tail');
-}
+    const changeLastLiText = (newText) => {
+        liElements[liElements.length - 1].textContent = newText;
+    }
 
-liText();
-getUlVal();
-getUlNames();
-changeLastLiText('Привіт, мене звуть Софія');
-addAttrToFirstLi('Софія');
-removeAttrFunc();
+    const addAttrToFirstLi = (name) => {
+        liElements[0].setAttribute('data-my-name', name);
+    }
 
+    const removeAttrFunc = () => {
+        ulElement.removeAttribute('data-dog-tail');
+    }
+
+    liText();
+    getUlVal();
+    getUlNames();
+    changeLastLiText('Привіт, мене звуть Софія');
+    addAttrToFirstLi('Софія');
+    removeAttrFunc();
+
+})();
 
 
 
