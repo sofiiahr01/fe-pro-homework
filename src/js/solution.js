890b695bbf8b8ca33a7efcb0660c247e71639344
sofiiahr01 <sgradinarova2001@gmail.com>
2024-10-08 14:57:27 +0300

    hw_35 completed

---
+++ src/js/solution.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const ulElement = document.querySelector('ul');
+const liElements = document.querySelectorAll('ul li');
+const ulAttr = ulElement.attributes;
+
+const liText = () => {
+    liElements.forEach((li) => {
+        console.log(li.textContent);
+    });
+}
+
+const getUlVal = () => {
+    const attrValues = [];
+
+    for (let attr of ulAttr) {
+        attrValues.push(attr.value);
+    }
+    console.log(attrValues);
+}
+
+const getUlNames = () => {
+    const attrNames = [];
+
+    for (let attr of ulAttr) {
+        attrNames.push(attr.name);
+    }
+    console.log(attrNames);
+}
+
+const changeLastLiText = (newText) => {
+    liElements[liElements.length - 1].textContent = newText;
+}
+
+const addAttrToFirstLi = (name) => {
+    liElements[0].setAttribute('data-my-name', name);
+}
+
+const removeAttrFunc = () => {
+    ulElement.removeAttribute('data-dog-tail');
+}
+
+liText();
+getUlVal();
+getUlNames();
+changeLastLiText('Привіт, мене звуть Софія');
+addAttrToFirstLi('Софія');
+removeAttrFunc();
+
+
+
+
+
+
+
