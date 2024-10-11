760cd774fd1b8778b2995b891653ac2c4f3cf178
sofiiahr01 <sgradinarova2001@gmail.com>
2024-10-11 10:53:38 +0300

    hw_36 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,36 @@
 'use strict';
 
-const userName = prompt('Enter user name')
+const LIST_TYPES = {
+    UL: 'ul',
+    OL: 'ol',
+    MENU: 'menu',
+}
 
-alert(`Hello ${userName}! How are you?`);
+const appendHTMLElement = (parentEl = null, elementToAppend = null) => {
+    if (!parentEl || !elementToAppend) return
+    parentEl.append(elementToAppend)
+}
+
+const generateList = (listItems, listType = LIST_TYPES.UL) => {
+    if (!Object.values(LIST_TYPES).includes(listType)) throw new Error('listType is must be a list type');
+    if (!listItems || !Array.isArray(listItems)) throw new Error('array is must be an array');
+    if (!listItems.length) throw new Error('array length shouldn`t be equal to 0')
+
+    const listElement = document.createElement(listType);
+
+    listItems.forEach((item) => {
+        const listItemElement = document.createElement('li');
+        appendHTMLElement(listElement, listItemElement);
+
+        if (Array.isArray(item)){
+            appendHTMLElement(listItemElement, generateList(item, listType))
+        }else {
+            listItemElement.innerHTML = item;
+        }
+        appendHTMLElement(listElement, listItemElement);
+    })
+    return listElement
+}
+
+const result = generateList([1, 2, 3, [4, 5, 6]], 'ol');
+appendHTMLElement(document.body, result)
