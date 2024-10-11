be1efa1f96a7e121a540f18a9d6c9e15422c7a4f
sofiiahr01 <sgradinarova2001@gmail.com>
2024-10-11 11:04:06 +0300

    hw_36 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,36 +1,39 @@
 'use strict';
 
-const LIST_TYPES = {
-    UL: 'ul',
-    OL: 'ol',
-    MENU: 'menu',
-}
+(function(){
+    const LIST_TYPES = {
+        UL: 'ul',
+        OL: 'ol',
+        MENU: 'menu',
+    }
 
-const appendHTMLElement = (parentEl = null, elementToAppend = null) => {
-    if (!parentEl || !elementToAppend) return
-    parentEl.append(elementToAppend)
-}
+    const appendHTMLElement = (parentEl = null, elementToAppend = null) => {
+        if (!parentEl || !elementToAppend) return
+        parentEl.append(elementToAppend)
+    }
 
-const generateList = (listItems, listType = LIST_TYPES.UL) => {
-    if (!Object.values(LIST_TYPES).includes(listType)) throw new Error('listType is must be a list type');
-    if (!listItems || !Array.isArray(listItems)) throw new Error('array is must be an array');
-    if (!listItems.length) throw new Error('array length shouldn`t be equal to 0')
+    const generateList = (listItems, listType = LIST_TYPES.UL) => {
+        if (!Object.values(LIST_TYPES).includes(listType)) throw new Error('listType is must be a list type');
+        if (!listItems || !Array.isArray(listItems)) throw new Error('array is must be an array');
+        if (!listItems.length) throw new Error('array length shouldn`t be equal to 0')
 
-    const listElement = document.createElement(listType);
+        const listElement = document.createElement(listType);
 
-    listItems.forEach((item) => {
-        const listItemElement = document.createElement('li');
-        appendHTMLElement(listElement, listItemElement);
+        listItems.forEach((item) => {
+            const listItemElement = document.createElement('li');
+            appendHTMLElement(listElement, listItemElement);
 
-        if (Array.isArray(item)){
-            appendHTMLElement(listItemElement, generateList(item, listType))
-        }else {
-            listItemElement.innerHTML = item;
-        }
-        appendHTMLElement(listElement, listItemElement);
-    })
-    return listElement
-}
+            if (Array.isArray(item)){
+                appendHTMLElement(listItemElement, generateList(item, listType))
+            }else {
+                listItemElement.innerHTML = item;
+            }
+            appendHTMLElement(listElement, listItemElement);
+        })
+        return listElement
+    }
 
-const result = generateList([1, 2, 3, [4, 5, 6]], 'ol');
-appendHTMLElement(document.body, result)
+    const result = generateList([1, 2, 3, [4, 5, 6]], 'ol');
+    appendHTMLElement(document.body, result)
+})();
+
