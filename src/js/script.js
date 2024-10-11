9a1aa60e971edff826f16463e23c59ca1cf574fa
sofiiahr01 <sgradinarova2001@gmail.com>
2024-10-11 17:36:01 +0300

    hw_37 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,79 @@
 'use strict';
 
-const userName = prompt('Enter user name')
+(function () {
+    const todoForm = document.querySelector('[data-todo-form]');
+    const todoListContainer = document.querySelector('#todoItems');
 
-alert(`Hello ${userName}! How are you?`);
+    const createTodoElement = ({ title, description }) => {
+        const wrapper = document.createElement('div');
+        wrapper.className = 'col-4';
+
+        wrapper.innerHTML = `
+            <div class="taskWrapper">
+                <div class="taskHeading">${title}</div>
+                <div class="taskDescription">${description}</div>
+            </div>
+        `;
+
+        return wrapper;
+    };
+
+    const getFormHandlers = (todoForm) => {
+        let isFormSubmitDisabled = true;
+        const fields = {};
+
+        const init = () => {
+            todoForm.querySelectorAll('input, textarea').forEach(({name}) => {
+                fields[name] = false;
+            })
+        }
+        init();
+
+        console.log(fields)
+
+        const handleSubmit = (event) => {
+            event.preventDefault();
+            if(isFormSubmitDisabled) return;
+
+            const inputs = event.target.querySelectorAll('input, textarea');
+            const data = Array.from(inputs).reduce((acc, { name, value }) => {
+                acc[name] = value;
+                return acc;
+            }, {});
+
+            const todoItemElement = createTodoElement(data);
+            todoListContainer.prepend(todoItemElement);
+            event.target.reset();
+        };
+
+        const inputHandler = ({target}) => {
+            const formSubmitBtn = todoForm.querySelector('button[type=submit]');
+
+            if(target.value.trim().length) {
+                if(!fields[target.name]) fields[target.name] = true;
+            } else {
+                if(fields[target.name]) fields[target.name] = false;
+            }
+
+            isFormSubmitDisabled = !Object.values(fields).every((field) => field);
+
+            if(!isFormSubmitDisabled) {
+                formSubmitBtn.removeAttribute('disabled')
+            } else {
+                formSubmitBtn.setAttribute('disabled', '')
+            }
+        }
+
+        return {
+            inputHandler,
+            handleSubmit
+        };
+    };
+
+    const { inputHandler, handleSubmit } = getFormHandlers(todoForm);
+
+    todoForm.addEventListener('input', inputHandler);
+    todoForm.addEventListener('submit', handleSubmit);
+
+})();
+
