caa883c36003edbd256d0ed9a7558323f9446aca
sofiiahr01 <sgradinarova2001@gmail.com>
2024-10-22 21:24:45 +0300

    hw_39 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,37 @@
 'use strict';
 
-const userName = prompt('Enter user name')
+(function () {
+    const deepFreeze = (obj) => {
+        Object.freeze(obj);
+        Object.keys(obj).forEach((key) => {
+            if (typeof obj[key] === 'object' && obj[key] !== null && !Object.isFrozen(obj[key])) {
+                deepFreeze(obj[key]);
+            }
+        });
+        return obj;
+    }
 
-alert(`Hello ${userName}! How are you?`);
+    let user = {
+        data: {
+            a: 1,
+            b: 2,
+            c: 3,
+            d: {
+                a1: 1,
+                b1: 2,
+                c1: 3,
+                d1: {
+                    a2: 3,
+                    b2: 3,
+                    c2: 3,
+                }
+            }
+        }
+    };
+
+    deepFreeze(user)
+
+    console.log(user.data.d.a1);
+    user.data.d.a1 = 4;
+    console.log(user.data.d.a1);
+})();
