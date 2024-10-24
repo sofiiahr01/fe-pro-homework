2519399ed52b30335a5cbba83755a3575aa7c62c
sofiiahr01 <sgradinarova2001@gmail.com>
2024-10-24 17:29:11 +0300

    hw_40 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,28 @@
 'use strict';
 
-const userName = prompt('Enter user name')
+(function (){
+    let user = {
+        _name: 'Yana',
+        _age: 22,
 
-alert(`Hello ${userName}! How are you?`);
+        get userName (){
+            return this._name
+        },
+        set userName (newName) {
+            this._name = newName
+        },
+
+        get userAge () {
+            return this._age
+        },
+        set userAge (value){
+            if (typeof value !== 'number' || value < 0 || value > 120) throw new Error('the number should`t be less than 0 and more than 120')
+            this._age = value
+        }
+    }
+
+        user.userName = 'Lily';
+        user.userAge = 122;
+
+        console.log(user);
+})()
