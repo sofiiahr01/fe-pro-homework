abf0b4f8c003cae3b1b5cd42a387c372e18c3e50
sofiiahr01 <sgradinarova2001@gmail.com>
2024-11-04 19:51:14 +0200

    hw_42 completed

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 import Student from "./Student.js";
 
-const studentInstance1 = new Student('John', 'Doe', 1997);
-const studentInstance2 = new Student('Alice', 'Cooper', 2000);
+const studentInstance1 = new Student('Yana', 'Repiah', 2002);
+const studentInstance2 = new Student('Alex', 'Kim', 2000);
 
 console.log(studentInstance1)
 console.log(studentInstance2)
